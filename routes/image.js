4e04a56455d7fd6f4d874d9f50be21c9bf901937
B3rn475 <b3rn4@hotmail.it>
2014-06-18 16:47:19 +0200

    Added filter for valid Tagging Actions for Image Tags computation

--- routes/image.js
+++ routes/image.js
@@ -88,7 +88,7 @@ exports.routes.count = function (req, res, next) {
 };
 
 exports.routes.tag = function (req, res, next) {
-    var match = { type: "tagging" },
+    var match = { type: "tagging", validity: true },
         group = {$group: {_id: "$tag"}},
         grouping = {$group: {_id: null, result: {$addToSet: "$_id"}}},
         cbNext = function (err, objects) {
